f77e87c2329836065bbe23ec644b1eac4426b7a1
pzzyf <2279948211@qq.com>
2023-12-08 11:30:43 +0800

    1

--- build/afe1-ui.mjs
+++ build/afe1-ui.mjs
@@ -1,1 +1,28 @@
-
+import { openBlock as r, createElementBlock as l } from "vue";
+const a = (o, t) => {
+  const n = o.__vccOpts || o;
+  for (const [e, s] of t)
+    n[e] = s;
+  return n;
+}, _ = {}, f = { class: "hhh" };
+function i(o, t) {
+  return r(), l("button", f);
+}
+const m = /* @__PURE__ */ a(_, [["render", i]]), u = [m], g = "a", c = "_Vue3UI", h = (o, t = { classPrefix: g }) => {
+  o.config.globalProperties[c] = {
+    ...o.config.globalProperties[c] ?? {},
+    classPrefix: t.classPrefix
+  };
+}, x = (o, t, n) => {
+  h(o, n), o.component(t.name, t);
+}, p = {
+  install(o, t) {
+    u.forEach((n) => {
+      x(o, n, t);
+    });
+  }
+};
+export {
+  m as Button,
+  p as default
+};
