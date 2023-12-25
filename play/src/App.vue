98151f9ffba6fddb919639eac7c38cce7ab526fb
pzzyf <2279948211@qq.com>
2023-12-25 09:29:54 +0800

    input的v-model

--- play/src/App.vue
+++ play/src/App.vue
@@ -9,11 +9,17 @@ import Base from '../../docs/examples/icon'
 <style scoped></style> -->
 
 <template>
-  <AInput placeholder="Please input"></AInput>
+  <input ref="input" :value="state" type="text" />
+  <button @click="handleClk">提交</button>
 </template>
 
 <script setup lang="ts">
-import AInput from '@afe1-ui/components/input'
+import { ref } from 'vue'
+const input = ref()
+const state = ref(666)
+const handleClk = () => {
+  console.log(input.value.value)
+}
 </script>
 
 <style scoped></style>
