881774bdbf0fbbef8a6847706fbeebd96ce46f1b
pzzyf <2279948211@qq.com>
2023-12-27 10:05:45 +0800

    button type prop

--- docs/examples/button/base.vue
+++ docs/examples/button/base.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>1</div>
+  <a-button>Default</a-button>
+  <a-button type="primary">Primary</a-button>
+  <a-button type="success">Success</a-button>
+  <a-button type="info">Info</a-button>
+  <a-button type="warning">Warning</a-button>
+  <a-button type="danger">Danger</a-button>
 </template>
 
 <script setup lang="ts">
+import AButton from '@afe1-ui/components/button'
+import '@afe1-ui/theme-chalk/src/button.scss'
 defineExpose({
   name: 'Base',
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.a-button {
+  margin-right: 6px;
+}
+</style>
--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    <button>1</button>
+  <div :class="buttonKls">
+    <slot />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useNamespace } from '@afe1-ui/hooks'
+import { buttonProps } from './button'
+const props = defineProps(buttonProps)
+const ns = useNamespace('button')
+const buttonKls = computed(() => [ns.b(), ns.m(props.type)])
+</script>
 
 <style scoped></style>
