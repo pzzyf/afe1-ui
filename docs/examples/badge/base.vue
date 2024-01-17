ea0f7e592a1d5f48a5c907071c6906aa047df13c
pzzyf <2279948211@qq.com>
2024-01-17 21:41:24 +0800

    wip: badge scss

--- docs/examples/badge/base.vue
+++ docs/examples/badge/base.vue
@@ -4,6 +4,7 @@
 
 <script setup lang="ts">
 import ABadge from '@afe1-ui/components/badge'
+import '@afe1-ui/theme-chalk/src/badge.scss'
 defineExpose({
   name: 'Base',
 })
--- packages/components/badge/src/badge.vue
+++ packages/components/badge/src/badge.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
+  <div :class="ns.b()">
     <slot />
-    <sup>{{ content }}</sup>
+    <sup v-text="content" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { defineOptions } from 'vue'
+import { useNamespace } from '@afe1-ui/hooks'
 import { badgeProps } from './badge'
 defineOptions({
   name: 'ABadge',
 })
 defineProps(badgeProps)
+const ns = useNamespace('badge')
 </script>
 
 <style scoped></style>
